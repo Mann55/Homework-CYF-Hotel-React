0ccef5a69e26e11ea16b24044e4a936841f7a338
manpreet <manhspman5@gmail.com>
2022-11-14 11:23:56 +0100

    cyf hotel exercises are done

--- src/components/Bookings.js
+++ src/components/Bookings.js
@@ -1,14 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Search from "./Search.js";
 import SearchResults from "./SearchResults.js";
-import FakeBookings from "../data/fakeBookings.json";
 import { useState } from "react";
 
 const Bookings = () => {
-  const [bookings, setBookings] = useState(FakeBookings);
+  const [bookings, setBookings] = useState([]);
+
   const search = (searchVal) => {
     console.info("TO DO!", searchVal);
+    const filteredBooking =
+      bookings.filter((booking) => booking.firstName == searchVal) ||
+      booking.surname == searchVal;
+
+    console.log(filteredBooking);
+
+    setBookings(filteredBooking);
   };
+
+  useEffect(() => {
+    fetch("https://cyf-react.glitch.me")
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+        throw new Error("Something went wrong");
+      })
+      //  .then((data) => console.log(data));
+      .then((data) => setBookings(data))
+      .catch((error) => console.log(error));
+  }, []);
 
   return (
     <div className="App-content">
--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,9 +1,13 @@
 import moment from "moment/moment";
-import React, { useState } from "react";
+import CustomerProfile from "./CustomerProfile";
+import React, { useEffect, useState } from "react";
 
 const SearchResults = (props) => {
   // console.log(props);
   const [clickedRow, SetClickedRow] = useState();
+
+  const [customerId, setCustomerId] = useState(-1);
+
   function handleRowClicked(index) {
     // which element is going to clicked
     //console.log("clicked");
@@ -11,43 +15,59 @@ const SearchResults = (props) => {
     console.log(clickedRow);
   }
 
+  const showProfile = (customerId) => {
+    setCustomerId(customerId);
+    //   console.log(customerId);
+
+    useEffect;
+  };
+
   return (
-    <table class="table">
-      <thead>
-        <tr>
-          <th scope="col">ID</th>
-          <th scope="col">Title</th>
-          <th scope="col">First-Name</th>
-          <th scope="col">Surname</th>
-          <th scope="col">Email</th>
-          <th scope="col">Room-ID</th>
-          <th scope="col">Check-In-Date</th>
-          <th scope="col">Check-Out-Date</th>
-          <th scope="col">NumberOfNights</th>
-        </tr>
-      </thead>
-      <tbody>
-        {props.results.map((result, index) => (
-          <tr
-            key={index}
-            onClick={() => handleRowClicked(index)}
-            className={clickedRow == index ? "highlight" : null}
-          >
-            {result.id}
-            <td>{result.title}</td>
-            <td>{result.firstName}</td>
-            <td>{result.surname}</td>
-            <td>{result.email}</td>
-            <td>{result.roomId}</td>
-            <td>{result.checkInDate}</td>
-            <td>{result.checkOutDate}</td>
-            <td>
-              {totalNumberOfNights(result.checkOutDate, result.checkInDate)}
-            </td>
+    <div>
+      <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Title</th>
+            <th scope="col">First-Name</th>
+            <th scope="col">Surname</th>
+            <th scope="col">Email</th>
+            <th scope="col">Room-ID</th>
+            <th scope="col">Check-In-Date</th>
+            <th scope="col">Check-Out-Date</th>
+            <th scope="col">NumberOfNights</th>
+            <th scope="col">Show Profile</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {props.results.map((result, index) => (
+            <tr
+              key={index}
+              onClick={() => handleRowClicked(index)}
+              className={clickedRow == index ? "highlight" : null}
+            >
+              {result.id}
+              <td>{result.title}</td>
+              <td>{result.firstName}</td>
+              <td>{result.surname}</td>
+              <td>{result.email}</td>
+              <td>{result.roomId}</td>
+              <td>{result.checkInDate}</td>
+              <td>{result.checkOutDate}</td>
+              <td>
+                {totalNumberOfNights(result.checkOutDate, result.checkInDate)}
+              </td>
+              <td>
+                <button onClick={() => showProfile(result.id)}>
+                  Show Profile
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      {customerId > -1 ? <CustomerProfile customerId={customerId} /> : null}
+    </div>
   );
 };
 
